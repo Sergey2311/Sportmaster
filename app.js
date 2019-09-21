65c9ebe0bee9fae567d654e1785399f0c53e4205
Student <student@itpark.ru>
2019-09-21 12:18:59 +0300

    Часть 2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('work');
+console.log('worked');
+
+const prevPurchases = 19000;
+const currentPurchase = 4000;
+
+let bonusesPerOneThousand;
+if (prevPurchases <= 15000) {
+    bonusesPerOneThousand = 50;
+} else {
+    if (prevPurchases <= 150000) {
+        bonusesPerOneThousand = 70;
+    } else {
+        bonusesPerOneThousand = 100; 
+    }
+}
+
+console.log(bonusesPerOneThousand);
